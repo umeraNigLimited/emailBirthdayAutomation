25be42dc102e54545c5c4ec72700202772489290
Apple <apple@Apples-MacBook-Pro.local>
2024-12-03 10:09:03 +0100

    Re-Tested the cron Job

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ const checkBirthdaysAndSendEmails = async () => {
 };
 
 cron.schedule(
-  "0 9 * * *", async () => {
+  "20 10 * * *", async () => {
     console.log('Email Schedule started to run')
     await checkBirthdaysAndSendEmails();
   }
